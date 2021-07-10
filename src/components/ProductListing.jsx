eb02d34955e885182d29eaf9905df3a0c36189d8
stardoxx <ashutoshsinghyadav03@gmail.com>
2021-07-10 23:24:38 +0530

    use axios for api call and fetching products from fake store api

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect}from 'react'
+import {useSelector} from 'react-redux';
+import ProductComponent from './ProductComponent';
+import axios from 'axios';
 
 const ProductListing = () => {
+
+    const products = useSelector((state) => state);
+
+    const fetchProducts = async () => {
+        const response = await axios.get('https://fakestoreapi.com/products')
+        .catch((error) => {
+            console.error(error);
+        });
+        console.log(response);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
     return (
-        <div>
+        <div className="ui grid container">
             <h1>product listing</h1>
+            <ProductComponent/>
         </div>
     )
 }
