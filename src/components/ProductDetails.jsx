39c48c21149c068f102c687fc08a13518597741b
stardoxx <ashutoshsinghyadav03@gmail.com>
2021-07-11 14:12:10 +0530

    used useParam hook for getting product id in productDetails component and add selectedProduct reducer

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { selectedProducts } from '../redux/actions/productActions';
 const ProductDetails = () => {
+    const product = useSelector((state) => state.product);
+    const {productId} = useParams();
+    const dispatch = useDispatch();
+   console.log(productId);
+
+    const fetchProductDetails = async () => {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
+        .catch((error) => {
+            console.log(error);
+        });
+
+        dispatch(selectedProducts(response.data));
+    }
+
+    useEffect(() => {
+        if(productId && productId !== '') fetchProductDetails();
+    },[productId]);
+
     return (
         <div>
              <h1>product dtails</h1>
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -8,11 +8,21 @@ const initialState = {
 
 export const productReducer = (state = initialState,{type, payload}) => {
     switch(type){
-        case ActionTypes.setProducts: 
+        case ActionTypes.SET_PRODUCTS: 
         return {...state,
             products: payload
         }
     default: 
         return state;
+    }
+}
+
+export const selectedProductsReducer = (state = {}, {type, payload}) => {
+    switch(type){
+        case ActionTypes.SELECTED_PRODUCT :
+            return {...state, ...payload};
+        default:
+            return state;
+
     }
 }
